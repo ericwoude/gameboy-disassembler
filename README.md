9ed5d716fb092af4f3c43dadd23275cbc2c8ff00
Eric van der Woude <ericvdwoude@gmail.com>
2022-10-16 17:13:41 +0200

    Added custom hex option formatter and updated README

--- README.md
+++ README.md
@@ -4,5 +4,31 @@
 # Game Boy disassembler
 > Disassembler for the Nintendo Gameboy DMG-01
 
+## Usage
+### Help
+```
+$ ./gameboy-disassembler --help
+Usage: gameboy-disassembler [OPTIONS] --file <FILE>
+
+Options:
+  -f, --file <FILE>      Relative path pointing to GameBoy ROM
+  -e, --entry <ENTRY>    Entry address for disassembly [default: 336]
+  -a, --amount <AMOUNT>  Amount of instructions to decode from starting point [default: 8]
+  -h, --help             Print help information
+```
+
+### Running the disassembler
+```
+$ ./gameboy-disassembler --file ./snake.gb --entry 0x150
+0x150 NOP
+0x151 DI
+0x152 LD SP, 0xFFFE
+0x155 LD B, 0x80
+0x157 LD C, 0x0
+0x159 LDH A, (0x44)
+0x15B CP 0x90
+0x15D JR NZ, 0xFA
+```
+
 
 Credits for the test rom go to Yvar de Goffau [snake.gb](https://forums.nesdev.org/viewtopic.php?f=20&t=16787)
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,34 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Args {
+    /// Relative path pointing to GameBoy ROM
     #[arg(short, long, required(true))]
     file: String,
 
-    #[arg(short, long, default_value_t = 0x150)]
+    /// Entry address for disassembly
+    #[arg(short, long, default_value_t = 0x150, value_parser = clap::builder::ValueParser::new(parse_hex_string))]
     entry: usize,
+
+    /// Amount of instructions to decode from starting point
+    #[arg(short, long, default_value_t = 8)]
+    amount: usize,
+}
+
+fn parse_hex_string(s: &str) -> Result<usize, std::io::Error> {
+    let mut base = 10;
+    let mut numbers = s;
+
+    if s.starts_with("0x") || s.starts_with("0X") {
+        base = 16;
+        numbers = &s[2..];
+    }
+
+    let result = usize::from_str_radix(numbers, base);
+    if result.is_err() {
+        panic!("error parsing entry value into int");
+    } else {
+        Ok(result.unwrap())
+    }
 }
 
 fn main() {
@@ -17,5 +40,5 @@ fn main() {
     let instructions = parse::parse_from_file("./instructions.json");
     let bytes = parse::load_cartridge(&args.file);
 
-    parse::disassemble(args.entry, &bytes, &instructions, 8);
+    parse::disassemble(args.entry, &bytes, &instructions, args.amount);
 }
